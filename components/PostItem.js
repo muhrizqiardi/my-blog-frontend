dfc266cbbeab7061cbc87e34f9f2a471268fcca4
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-05-19 17:01:49 +0700

    Handle null username

--- components/PostItem.js
+++ components/PostItem.js
@@ -6,7 +6,7 @@ function PostItem(props) {
       <div className="post-item">
         <a className="post-item-anchor" href={props.href}>
           <h2>{props.title}</h2>
-          <span>By {props.author} at {new Date(Date.parse(props.date)).toLocaleString()} {props.categories ? ",":" "} {props.categories && props.categories.map((category) => <a href="#">#{category.Name} </a>)}</span>
+          <span>By {props.author ? props.author : "Unknown"} at {new Date(Date.parse(props.date)).toLocaleString()}{props.categories ? ",":" "} {props.categories && props.categories.map((category) => <a href="#">#{category.Name} </a>)}</span>
           <p>{props.content}</p>
         </a>
       </div>
